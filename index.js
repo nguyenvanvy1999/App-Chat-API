1a337d9c9545ad90a6f5ce2d7fb09149810e2d34
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-01-19 00:08:24 +0700

    Config and start server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const server = require('http').createServer(app);
+const mongo = require('./config/setting/mongo/index');
+const morgan = require('morgan');
+const serverConfig = require('./config/constant/server');
+const UserRouter = require('./user/user.router')();
+// ________________________________________________
+mongo.connectMongo();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/uploads', express.static('uploads'));
+// ________________________________________________
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        req.headers('Access-Control-Allow-Methods', 'PUT,POST,PATH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+// ________________________________________________
+app.use('/user', UserRouter);
+// ________________________________________________
+server.listen(serverConfig.port, serverConfig.host, () => {
+    console.log(
+        'server on: http://' +
+        server.address().address +
+        ':' +
+        server.address().port
+    );
+});
