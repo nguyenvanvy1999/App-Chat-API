126cab767f70439e7810de54bca50f881d349749
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-01-20 16:25:23 +0700

    Add  error handler to project

---
+++ middleware/error.js
@@ -0,0 +1,47 @@
+const { errorHandler, APIError } = require('../helper/error');
+const logger = require('../helper/winston');
+const HTTP_STATUS_CODE = require('../config/constant/http');
+
+function handleNotFoundPage(req, res) {
+    return res
+        .status(HTTP_STATUS_CODE.ERROR.NOT_FOUND)
+        .json({ message: 'Page not found' });
+}
+
+// Error handler
+function handleError(err, req, res, next) {
+    logger.error(err);
+    if (!errorHandler.isTrustedError(err)) {
+        return res.status(HTTP_STATUS_CODE.ERROR.BAD_REQUEST).send({
+            message: 'Something was wrong. Please contact me to fix. Thank you!',
+        });
+    }
+    if (err.name === 'MongoError' && err.code === 11000) {
+        let [_, collection, field, value] = err.message.match(
+            /collection: [a-z]*\.([a-z]*)\sindex:\s([a-z]+).*{\s?[a-zA-z0-9]*:\s?"?([a-z0-9@. ]+)"?/i
+        );
+        err = new APIError({
+            message: ` ${collection} exist ${field} : ${value}`,
+            status: 409,
+        });
+    }
+    if (err.name === 'MulterError') {
+        err = new APIError({
+            message: err.message,
+            status: err.status || HTTP_STATUS_CODE.SERVER_ERROR.SERVICE_UNAVAILABLE,
+        });
+    }
+    if (err) {
+        err = new APIError({
+            message: err.message,
+            status: err.status || HTTP_STATUS_CODE.SERVER_ERROR.SERVICE_UNAVAILABLE,
+            errors: err.error,
+        });
+    }
+    return res.status(err.status).json(err);
+}
+
+module.exports = {
+    handleNotFoundPage,
+    handleError,
+};
