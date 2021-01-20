a76a4d4132840a86426bf7d28ab2d5d82e5b2776
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-01-20 19:52:47 +0700

    add bcryptHelper

---
+++ helper/bcrypt.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+const { APIError } = require('./error');
+
+async function hash(password, saltRounds) {
+    try {
+        const result = await bcrypt.hash(password, saltRounds);
+        return result;
+    } catch (error) {
+        throw new APIError({ message: error.message, errors: error });
+    }
+}
+async function compare(password, hash) {
+    try {
+        const result = await bcrypt.compare(password, hash);
+        return result;
+    } catch (error) {
+        throw new APIError({ message: error.message, errors: error });
+    }
+}
+
+module.exports = { hash, compare };
