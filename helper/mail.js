65c2ba93d60a6e0436f3f7eca9d93b91bb294446
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-01-20 15:59:33 +0700

    add Send mail using Nodemailer

---
+++ helper/mail.js
@@ -0,0 +1,26 @@
+const nodeMailer = require('nodemailer');
+const mailConfig = require('../config/constant/mail').mailConfig;
+const mailOption = require('../config/constant/mail').mailOption;
+const { APIError } = require('../helper/error');
+
+function newMailOption(to, text) {
+    const newOption = {
+        from: mailConfig.auth.user,
+        to: to,
+        subject: mailOption.subject,
+        text: mailOption.text + text + mailOption.text2,
+    };
+    return newOption;
+}
+
+async function sendMail(mailOption) {
+    try {
+        const transporter = nodeMailer.createTransport(mailConfig);
+        const mail = await transporter.sendMail(mailOption);
+        return mail;
+    } catch (error) {
+        throw new APIError({ message: error.message, errors: error });
+    }
+}
+
+module.exports = { newMailOption, sendMail };
