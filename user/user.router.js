b07ca64a035b7f828aa6eb361a75cb87f77745e9
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-01-22 19:37:48 +0700

    add joi middleware to user routers

--- user/user.router.js
+++ user/user.router.js
@@ -3,13 +3,20 @@ const router = express.Router();
 const multer = require('multer');
 const UserController = require('./user.controller');
 const handleError = require('../middleware/error').handleError;
+const UserValidate = require('../middleware/joi');
 
 module.exports = () => {
     router.use(multer().none());
-    router.route('/sign-up').post(UserController.signUp, handleError);
-    router.route('/verify').get(UserController.verifyAccount, handleError);
+    router
+        .route('/sign-up')
+        .post(UserValidate.joiSignUp, UserController.signUp, handleError);
+    router
+        .route('/verify')
+        .get(UserValidate.joiToken, UserController.verifyAccount, handleError);
     router.route('/search/user?').get(UserController.findUser, handleError);
-    router.route('/sign-in').get(UserController.signIn, handleError);
+    router
+        .route('/sign-in')
+        .get(UserValidate.joiSignIn, UserController.signIn, handleError);
     router.route('/:id').get(UserController.userProfile, handleError);
     return router;
 };
