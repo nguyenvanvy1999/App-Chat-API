84b43c2f68eed87c18929a8239f8104fba4e6f11
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-01-23 19:57:02 +0700

    edit message model

--- message/message.model.js
+++ message/message.model.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const ObjectId = Schema.Types.ObjectId;
 const MessageSchema = new Schema({
-    _id: Schema.Types.ObjectId,
-    from: { type: Schema.Types.ObjectId, ref: 'User' },
-    to: { type: Schema.Types.ObjectId, ref: 'User' },
-    properties: {
-        type: ['String', 'VoiceChat', 'File'],
-        content: String,
-        sendAt: { type: Date, default: Date.now },
-        idRead: { type: Boolean, default: false },
-        readAt: { type: Date, default: Date.now },
-    },
-});
+    _id: ObjectId,
+    from: { type: ObjectId, ref: 'User' },
+    to: { type: ObjectId, ref: 'Room' },
+    type: ['Text', 'Voice', 'Link', 'File'],
+    message: String,
+    status: { type: Boolean, default: true },
+    readAt: { type: Date, default: Date.now },
+}, { timestamps: true });
 
 const Message = mongoose.model('Message', MessageSchema);
 module.exports = Message;
